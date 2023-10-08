61de2c8f7974a77b0445466d205f55940b979155
asteve12335 <asteve12335@gmail.com>
2023-10-08 00:26:27 +0000

    LockBoxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 "LockBoxes interview question"
 
+
 def canUnlockAll(boxes):
     """Determines if all the boxes can be opened."""
     # Check if the input list is None or empty.
