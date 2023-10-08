2d93bb25c7e3c5d1533948dc24208cef9a30d20a
@steve <asteve12335@gmail.com>
2023-10-08 01:05:13 +0100

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"LockBoxes interview question"
+
+def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened."""
+    # Check if the input list is None or empty.
+    if boxes is None or len(boxes) == 0:
+        return False
+
+    # Create a list of keys, starting with the key to the first box.
+    keys = [0]
+
+    # Iterate over the list of keys, opening each box in turn.
+    for key in keys:
+        # Open the box with the current key.
+        box = boxes[key]
+
+        # Check if the key to the current box is already in the list of keys.
+        if box not in keys:
+            # If it is not, then add it to the list of keys.
+            keys.append(box)
+
+    # If the function was able to open all the boxes, then return True.
+    if len(keys) == len(boxes):
+        return True
+
+    # Otherwise, return False.
+    else:
+        return False
