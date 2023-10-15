d793507a65bf46764f03cdbc313479eb6dc53cdd
@steve <asteve12335@gmail.com>
2023-10-16 00:56:32 +0100

    min ops

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,3 +1,20 @@
+#!/bin/usr/env python3
+
+"""
+Prototype: def minOperations(n)
+Returns an integer
+If n is impossible to achieve, return 0
+Example:
+
+n = 9
+
+H => Copy All => Paste => HH => Paste =>HHH => Copy All => Paste => HHHHHH =>
+Paste => HHHHHHHHH
+
+Number of operations: 6
+"""
+
+
 def minOperations(n):
     """Calculates the fewest number of operations needed to result in
     exactly n H characters in the file.
