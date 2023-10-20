87dd3f85eda87aedd170bc0079f6d2b0a7294a38
@steve <asteve12335@gmail.com>
2023-10-20 03:01:26 +0100

    log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Script that reads stdin line by line and computes
+some statistics.
+"""
+
+
+import sys
+
+
+def print_statistics(file_size, status_code):
+    """
+    Prints the statistics
+    """
+    print("File size: {}".format(file_size))
+    for key, value in sorted(status_code.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
+
+
+if __name__ == "__main__":
+    file_size = 0
+    status_code = {"200": 0, "301": 0, "400": 0, "401": 0,
+                   "403": 0, "404": 0, "405": 0, "500": 0}
+    counter = 0
+    try:
+        for line in sys.stdin:
+            counter += 1
+            data = line.split()
+            file_size += int(data[-1])
+            if data[-2] in status_code:
+                status_code[data[-2]] += 1
+            if counter % 10 == 0:
+                print_statistics(file_size, status_code)
+        print_statistics(file_size, status_code)
+    except KeyboardInterrupt:
+        print("File size: {}".format(file_size))
+        for key, value in sorted(status_code.items()):
+            if value != 0:
+                print("{}: {}".format(key, value))
+        raise
